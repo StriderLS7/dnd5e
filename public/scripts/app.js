178bc300bae4fa1fc91da493d3d6ad6087e7c002
Lucas Schroeder <striderls7@hotmail.com>
2014-07-08 15:16:56 -0500

    Updated launch page - added angular-ui

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,28 +1,11 @@
 (function() {
-  var app = angular.module('DND5ETools', []);
+  var app = angular.module('DND5ETools', ['ui.bootstrap']);
 
   app.controller('MainController', function($scope, $http) {
-    this.__divShown = [];
     $scope.bestiary = [];
 
-    $http.get('http://localhost:8080/bestiary').success(function(data){
+    $http.get('http://localhost:8080/bestiary/monster').success(function(data){
       $scope.bestiary = data;
     });
-
-    console.log($scope.bestiary);
-
-    this.showDiv = function(name)
-    {
-       this.__divShown[name] = true;
-      console.log(this.__divShown);
-      console.log($scope.bestiary);
-      console.log($scope.bestiary[0].name)
-    };
-
-    this.divShown = function(name)
-    {
-      console.log("Check "+name + ": " + (this.__divShown[name] === true));
-      return this.__divShown[name] === true;
-    };
   });
 })();
